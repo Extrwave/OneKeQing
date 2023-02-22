580e95723faa55a7573d8248c1883cc865d3fda8
Extrwave <exwave@foxmail.com>
2023-02-22 17:07:04 +0800

    feat: 新增其他版块帖子浏览，以及帖子数量修改为10

--- src/plugins/genshin/utils/region.ts
+++ src/plugins/genshin/utils/region.ts
@@ -19,3 +19,29 @@ export function getRegion( first: string ): string {
 			return "unknown";
 	}
 }
+
+/**
+ * 获取游戏分区的ForumId
+ *
+ * @param gids 分区ID
+ */
+export function getBBSItemForumIds( gids: number ) {
+	switch ( gids ) {
+		case 1:
+			return [ 1, 14, 6, 4, 41, ];
+		case 2:
+			return [ 26, 43, 28, 29, 49, 50, ];
+		case 3:
+			return [ 30, 51, 31, 40, 32 ];
+		case 5:
+			return [ 54, 35, 34, 39, 47, 48, 55, 36 ]
+		case 4:
+			return [ 37, 60, 42, 38, 33 ];
+		case 6:
+			return [ 52, 61, 53, 56 ];
+		case 8:
+			return [ 57, 58, 59, ];
+		default:
+			return [ 26, 43, 28, 29, 49, 50, ];
+	}
+}
